80aa5930d630bad5e3d4a451681e7f3e94dc639c
Ashiqur Rahman Zeeshan <ashiq.rahman1337@gmail.com>
2024-01-11 06:23:40 +0600

    prepared changes for mongodb 7

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,21 @@
 const mongoose = require('mongoose')
 
-const connectDB = (url) => {
-  return mongoose.connect(url)
+mongoose.set('strictQuery', false) // Prepare for Mongoose 7
+
+const connectDB = async (url) => {
+  try {
+    await mongoose.connect(url)
+    console.log('Connected to MongoDB Serverless Instance')
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err)
+  }
 }
 
 module.exports = connectDB
+
+
+// const connectDB = (url) => {
+//   return mongoose.connect(url)
+// }
+
+// module.exports = connectDB
