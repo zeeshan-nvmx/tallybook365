861dfc7cd67d826a8506ca1483eb0c7c05a445eb
Ashiqur Rahman Zeeshan <ashiq.rahman1337@gmail.com>
2024-01-21 16:10:11 +0600

    complete validation remove

--- controllers/quoteController.js
+++ controllers/quoteController.js
@@ -73,14 +73,14 @@ const createQuoteSchema = z.object({
 })
 
 async function createQuote(req, res) {
-  validateRequestFields(req.body, ['user_id', 'mother_company', 'client_id', 'client_name', 'client_address', 'title', 'job_no', 'items', 'job_type', 'grand_total'])
+  // validateRequestFields(req.body, ['user_id', 'mother_company', 'client_id', 'client_name', 'client_address', 'title', 'job_no', 'items', 'job_type', 'grand_total'])
 
   // const validationResult = createQuoteSchema.safeParse(req.body)
   // if (!validationResult.success) {
   //   throw new BadRequestError(generateDefaultErrorMessage(validationResult.error.issues))
   // }
 
-  const { user_id, mother_company, invoice_id, chalan_id, purchaseOrder_id, client_id, client_name, client_address, title, job_no, date, items, vat, asf, t_and_c, bank_account, bank_name_address, swift, routing_no, brand, job_type, grand_total } = validationResult.data
+  const { user_id, mother_company, invoice_id, chalan_id, purchaseOrder_id, client_id, client_name, client_address, title, job_no, date, items, vat, asf, t_and_c, bank_account, bank_name_address, swift, routing_no, brand, job_type, grand_total } = req.body
 
   const quote = await Quote.create({ user_id, mother_company, invoice_id, chalan_id, purchaseOrder_id, client_id, client_name, client_address, title, job_no, date, items, vat, asf, t_and_c, bank_account, bank_name_address, swift, routing_no, brand, job_type, grand_total })
 
